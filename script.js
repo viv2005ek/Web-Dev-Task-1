e31c35c58020fa57873c3baed1a65dadd0fbf9f4
vivek <viv2005ek@gmail.com>
2024-06-07 02:17:36 +0530

    final

---
+++ script.js
@@ -0,0 +1,12 @@
+const navMenu = document.body.querySelector(".navigationMenu");
+
+
+
+function menu() {
+    navMenu.classList.toggle("visible");
+    if (navMenu.classList.contains("visible")) {
+        document.body.style.overflow = "hidden";
+    } else {
+        document.body.style.overflow = "";
+    }
+}
